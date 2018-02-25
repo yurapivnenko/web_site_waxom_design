3d5500a35ab46a01d913bb304d90cde363d50e2c
Yuri <36759251+yurapivnenko@users.noreply.github.com>
2018-02-25 18:15:52 +0200

    first commit

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,6 @@ $(function(){
 	});
 
 	$('.recent-posts__slider').slick({
-		draggable: false,
 		appendArrows: '.recent-post__buttons',
 		prevArrow: '<button type="button" class="slick-prev"><span class="icon-arrow_left"><span class="icon-left"><span class="path1"></span><span class="path2"></span></span></button>',
 		nextArrow: '<button type="button" class="slick-next"><span class="icon-arrow_right"><span class="icon-right"><span class="path1"></span><span class="path2"></span></span></button>',
